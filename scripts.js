6de2f6834d8f882d17731e3d996991ec103e5802
pacificRailway <kmichalson1@gmail.com>
2024-05-20 00:16:59 -0700

    2

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function showYear(year) {
+    // This function will handle displaying the map and information for the specified year.
+    // For now, it will just log the year to the console.
+    console.log(`Showing information for the year ${year}`);
+    // Future code will update the map with historical details and lines for the selected year.
+}
